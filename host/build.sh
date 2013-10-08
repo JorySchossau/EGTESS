87b36487588afe2f6178ce0a09998d4c76685436
GrumblyGibson <jory@msu.edu>
2013-10-08 11:01:43 -0400

    build script (now with options!)

--- host/build.sh
+++ host/build.sh
@@ -2,36 +2,61 @@
 
 mkdir -p bin
 
-EXENAME=
-DEFAULT_NAME="localmu"
+DEFAULT_NAME="game"
+EXENAME=$DEFAULT_NAME
 EXTENSION=""
 COMPILER="g++"
 
+GENES=2
+GENES_DEFINE="#define GENES"
+GENES_DECLARATION="$GENES_DEFINE 2"
+
 help() {
 	echo
-	echo Usage: "./$(basename $0) [win32] [binaryName]"
+	echo Usage: "./$(basename $0) [-hwg]"
+	echo "       -h help"
+	echo "       -w build win32 using mingw32"
+	echo "       -g<#genes> change default number of genes (only 2,3 are valid yet)"
 	echo
 }
 
-if [ "$1" == "--help" ]; then
-	help
-	exit
-fi
+while getopts "hwg:" OPTIONS; do
+	case $OPTIONS in
+		h) help;;
+		w)
+			COMPILER="i386-mingw32-g++"
+			EXTENSION=".exe"
+			EXENAME=$EXENAME.w32
+			;;
+		g) 
+			GENES=$OPTARG
+			EXENAME=$EXENAME.g$GENES
+			;;
+		?)
+			echo "Unknown option: $OPTARG"
+			exit 1
+			;;
+	esac
+done
 
-if [ -z $1 ]; then
-	EXENAME="$DEFAULT_NAME"
-else
-	if [ "$1" == "win32" ]; then
-		COMPILER="i386-mingw32-g++"
-		EXTENSION=".exe"
-		if [ -z $2 ]; then
-			EXENAME="$DEFAULT_NAME"
-		else
-			EXENAME="$DEFAULT_NAME.$2"
-		fi
-	else
-		EXENAME="$DEFAULT_NAME.$1"
-	fi
-fi
+#if [ -z $1 ]; then
+#	EXENAME="$DEFAULT_NAME"
+#else
+#	if [ "$1" == "win32" ]; then
+#		COMPILER="i386-mingw32-g++"
+#		EXTENSION=".exe"
+#		if [ -z $2 ]; then
+#			EXENAME="$DEFAULT_NAME"
+#		else
+#			EXENAME="$DEFAULT_NAME.$2"
+#		fi
+#	else
+#		EXENAME="$DEFAULT_NAME.$1"
+#	fi
+#fi
+
+sed -ie "s/$GENES_DECLARATION/$GENES_DEFINE $GENES/" src/main.cpp
 
 $COMPILER -O3 -o bin/$EXENAME$EXTENSION src/main.cpp;
+
+sed -ie "s/$GENES_DEFINE $GENES/$GENES_DECLARATION/" src/main.cpp
