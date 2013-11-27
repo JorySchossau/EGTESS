19d29e88f56ecb3ded64fb05ddc42ccac4f6215c
Jorden Schossau - Michigan State University - 2012-10-25 195803 <jory@osg-xsede.grid.iu.edu>
2013-11-27 01:26:55 +0000

    updated run.sh

--- host/run.sh
+++ host/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-./game.sh $(python makeNeutralTable.py 0.5) game.w32.m0.exe gb0.5m0 100000 100
-./game.sh $(python makeNeutralTable.py 0.5) game.w32.m1.exe gb0.5m1 100000 100
-./game.sh $(python makeNeutralTable.py 0.5) game.w32.m2.exe gb0.5m2 100000 100
-./game.sh $(python makeNeutralTable.py 0.5) game.w32.m3.exe gb0.5m3 100000 100
-./game.sh $(python makeNeutralTable.py 0.5) game.w32.m4.exe gb0.5m4 100000 100
-./game.sh $(python makeNeutralTable.py 0.5) game.w32.m5.exe gb0.5m5 100000 100
+## permutations
+./builg.sh -g2 -p0
+#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m0 gb0.5m0 100000 100
+#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m1 gb0.5m1 100000 100
+#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m2 gb0.5m2 100000 100
+#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m3 gb0.5m3 100000 100
+#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m4 gb0.5m4 100000 100
+#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m5 gb0.5m5 100000 100
