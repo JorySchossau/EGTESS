5fb856439e8a584cf0446863c7bcea6adf52ff42
GrumblyGibson <jory@msu.edu>
2013-12-12 17:06:57 -0500

    fixed bug where there was no normalization in the 2-space to 3-space probability mapping.

--- host/src/main.cpp
+++ host/src/main.cpp
@@ -371,6 +371,12 @@ int main(int argc, const char * argv[])
 				p[0]=genome[0];
 				p[1]=genome[1];
 				p[2]=genome[2];
+				s=p[0]+p[1]+p[2];
+				if(s==0.0){
+					// don't do anything (code was here)
+				} else
+					for(i=0;i<3;i++)
+						p[i]/=s;
 			} else {
 				switch(MAPPING) {
 					case 0:
@@ -404,19 +410,13 @@ int main(int argc, const char * argv[])
 						p[2]=genome[0]*(1.0-genome[1]);
 						break;
 				}
-			}
-			s=p[0]+p[1]+p[2];
-			if(s==0.0){
-				for(i=0;i<3;i++)
-				{
-					p[i]=randDouble;
+				if (p[0]+p[1] != 0.0) {
+					s = p[0] + p[1] - p[0]*p[1];
+					p[0] = p[0]/s;
+					p[1] = p[1]/s;
+					p[2] = p[2]/s;
 				}
-				s=p[0]+p[1]+p[2];
-				for(i=0;i<3;i++)
-					p[i]/=s;
-			} else
-				for(i=0;i<3;i++)
-					p[i]/=s;
+			}
 		}
 
 
