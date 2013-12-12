0a14ea863a430604630e1f17ea16a7dd4b1bcf83
Jorden Schossau - Michigan State University - 2012-10-25 195803 <jory@osg-xsede.grid.iu.edu>
2013-12-12 22:31:12 +0000

    validate previous results for spiking behavior.

--- host/run.sh
+++ host/run.sh
@@ -7,9 +7,16 @@
 #./build.sh -g2 -p3
 #./build.sh -g2 -p4
 #./build.sh -g2 -p5
-#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m0 gb0.5m0 100000 100
-#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m1 gb0.5m1 100000 100
-#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m2 gb0.5m2 100000 100
-#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m3 gb0.5m3 100000 100
-#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m4 gb0.5m4 100000 100
-#./game.sh $(python makeNeutralTable.py 0.5) game.g2.m5 gb0.5m5 100000 100
+./build.sh -g2
+./game.sh $(python makeNeutralTable.py 0.1) game.g2 gb0.1 100000 1000
+./game.sh $(python makeNeutralTable.py 0.3) game.g2 gb0.3 100000 1000
+./game.sh $(python makeNeutralTable.py 0.5) game.g2 gb0.5 100000 1000
+./game.sh $(python makeNeutralTable.py 0.7) game.g2 gb0.7 100000 1000
+./game.sh $(python makeNeutralTable.py 0.9) game.g2 gb0.9 100000 1000
+
+./build.sh -g2 -l
+./game.sh $(python makeNeutralTable.py 0.8) game.g2.l gb0.8l 100000 1000 0.02 0.1
+./game.sh $(python makeNeutralTable.py 0.8) game.g2.l gb0.8l 100000 1000 0.02 0.3
+./game.sh $(python makeNeutralTable.py 0.8) game.g2.l gb0.8l 100000 1000 0.02 0.5
+./game.sh $(python makeNeutralTable.py 0.8) game.g2.l gb0.8l 100000 1000 0.02 0.7
+./game.sh $(python makeNeutralTable.py 0.8) game.g2.l gb0.8l 100000 1000 0.02 0.9
