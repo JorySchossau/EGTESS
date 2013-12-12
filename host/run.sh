2429c2e86845a2e8a4dfadc7e5b18b9cf28f640d
Jorden Schossau - Michigan State University - 2012-10-25 195803 <jory@osg-xsede.grid.iu.edu>
2013-12-12 22:12:22 +0000

    modified run.sh

--- host/run.sh
+++ host/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 ## permutations
-./builg.sh -g2 -p0
+#./build.sh -g2 -p0
+#./build.sh -g2 -p1
+#./build.sh -g2 -p2
+#./build.sh -g2 -p3
+#./build.sh -g2 -p4
+#./build.sh -g2 -p5
 #./game.sh $(python makeNeutralTable.py 0.5) game.g2.m0 gb0.5m0 100000 100
 #./game.sh $(python makeNeutralTable.py 0.5) game.g2.m1 gb0.5m1 100000 100
 #./game.sh $(python makeNeutralTable.py 0.5) game.g2.m2 gb0.5m2 100000 100
